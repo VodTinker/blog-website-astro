54a21b879c857faa358bc966700086ec2329fb70
VodTinker <85367044+VodTinker@users.noreply.github.com>
2026-01-20 21:59:19 +0100

    -Fix error with accent

--- src/config.ts
+++ src/config.ts
@@ -188,14 +188,14 @@ export const siteConfig: SiteConfig = {
 			fontFamily: "ZenMaruGothic-Medium",
 			fontWeight: "400",
 			localFonts: ["ZenMaruGothic-Medium.ttf"],
-			enableCompress: true, // 启用字体子集优化，减少字体文件大小
+			enableCompress: false, // Deactivate for including accented characters of Spanish 
 		},
 		cjkFont: {
 			// 中日韩字体 - 作为回退字体
 			fontFamily: "萝莉体 第二版",
 			fontWeight: "500",
 			localFonts: ["萝莉体 第二版.ttf"],
-			enableCompress: true, // 启用字体子集优化，减少字体文件大小
+			enableCompress: true, // Activate for including accented characters of Spanish 
 		},
 	},
 	showLastModified: true, // 控制“上次编辑”卡片显示的开关
